b68f7b4db0ac8e1b8e49bc1edd8cebf5026aadbd
Gio <gioremanente13@gmail.com>
2024-06-13 04:30:47 +0800

    Add project files.

---
+++ BattleshipLite/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+            Console.ReadLine();
+        }
+
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Battleship Lite");
+            Console.WriteLine("**************************");
+        }
+    }
+}
